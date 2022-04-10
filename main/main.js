d7085833db5839c9a9cb9f2c5eba37fb7c0b72f1
yuliuz030 <julianquintero0830@gmail.com>
2022-04-09 22:56:12 -0500

    Corrección de validación

--- main/main.js
+++ main/main.js
@@ -8,45 +8,52 @@ const $btn4 = document.getElementById("btn4");
 document.addEventListener("input", e =>{
     if (e.target === $numero1 || e.target === $numero2) {
         console.log(Math.round($numero1.value)) || console.log(Math.round($numero2.value))
-        if (Math.sign($numero1.value) == 1 || Math.sign($numero2.value) === 1) {
-            console.log("validacion completa")
+        if(Math.sign($numero1.value) === 1) {
+            console.log("validacion completa");
         }
         else{
-            console.log("Numeros negativos no se aceptan")
-        }
+            console.log("Numeros negativos no se aceptan");
+        }   
     }
     else{
-        return null
+        return null;
     }
 })
 
+function validar(numero1 , numero2){
+    if((numero1.length) == ""){
+        alert("No has ingresado el primer numero");
+    }
+    if((numero2.length) == ""){
+        alert("No has ingresado el segundo numero");
+    }
+}
+
 function valorMenor(numero1 , numero2 ) {
-        while (numero1 > numero2) {
-            alert(`${numero2} es menor`)
-            break;
-        }
-        while (numero1 < numero2) {
-            alert(`${numero1} es menor`)
-            break;
-        }
-        while (numero1 === numero2) {
-            alert("Son iguales")
-            break;
-        }
+    while (numero1 > numero2) {
+        alert(`${numero2} es menor`);
+        break;
+    }
+    while (numero1 < numero2) {
+        alert(`${numero1} es menor`);
+        break;
+    }
+    while (numero1 === numero2) {
+        alert("Son iguales");
+        break;
+    }
 
 }
 
 function sonIguales(numero1 , numero2) {
 
     if (numero1 > numero2) {
-        alert(`los numeros ${numero1} y ${numero2} no son iguales`)
+        alert(`los numeros ${numero1} y ${numero2} no son iguales`);
     }
     else if (numero1 < numero2) {
-        alert(`los numeros ${numero1} y ${numero2} no son iguales`)
-        
+        alert(`los numeros ${numero1} y ${numero2} no son iguales`);
     } else {
-        alert("los numeros son iguales")
-        
+        alert("los numeros son iguales");
     }
 }
 
@@ -55,8 +62,8 @@ function intercambiar(numero1 , numero2) {
     numero2 = numero1 ^ numero2
     numero1 = numero1 ^ numero2
 
-    alert(`${numero1} es ahora:` + " " + numero2 )
-    alert(`${numero2} es ahora:`+ " " + numero1 )
+    alert(`${numero1} es ahora:` + " " + numero2 );
+    alert(`${numero2} es ahora:`+ " " + numero1 );
 }
 
 function sumar(numero1, numero2) {
@@ -65,36 +72,40 @@ function sumar(numero1, numero2) {
     }
     else{
         suma = parseFloat(numero1)+ parseFloat(numero2);
-        alert(`la suma es ${suma}`)
+        alert(`la suma es ${suma}`);
     }
 
 }
 
 document.addEventListener("click", e => {
-    e.preventDefault()   
+    e.preventDefault()
     if (e.target === $btn1){
-        valorMenor($numero1.value, $numero2.value)
+        validar($numero1.value, $numero2.value);
+        valorMenor($numero1.value, $numero2.value);
     }
 })
 
 document.addEventListener("click", e => {
     e.preventDefault()   
     if (e.target === $btn2){
-        sonIguales($numero1.value, $numero2.value)
+        validar($numero1.value, $numero2.value);
+        sonIguales($numero1.value, $numero2.value);
     }
 })
 
 document.addEventListener("click", e => {
     e.preventDefault()   
     if (e.target === $btn3){
-        intercambiar($numero1.value, $numero2.value)
+        validar($numero1.value, $numero2.value);
+        intercambiar($numero1.value, $numero2.value);
     }
 })
 
 document.addEventListener("click", e => {
-    e.preventDefault()   
+    e.preventDefault()  
     if (e.target === $btn4){
-        sumar($numero1.value, $numero2.value)
+        validar($numero1.value, $numero2.value);
+        sumar($numero1.value, $numero2.value);
     }
 })
  
